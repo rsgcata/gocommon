73291ba87585408e608723fb13578a18df428316
rsgcata <rsgcata@users.noreply.github.com>
2025-05-25 20:18:01 +0300

    Added IP logging as optional to access log

--- infrastructure/http/router/middleware/access.go
+++ infrastructure/http/router/middleware/access.go
@@ -15,10 +15,19 @@ const AccessLogMessage = "HTTP Request"
 type HttpAccessLogger struct {
 	nextHandler http.Handler
 	logger      *slog.Logger
+	options     AccessLogOptions
 }
 
-func NewHttpAccessLogger(next http.Handler, logger *slog.Logger) *HttpAccessLogger {
-	return &HttpAccessLogger{next, logger}
+type AccessLogOptions struct {
+	LogClientIp bool
+}
+
+func NewHttpAccessLogger(
+	next http.Handler,
+	logger *slog.Logger,
+	options AccessLogOptions,
+) *HttpAccessLogger {
+	return &HttpAccessLogger{next, logger, options}
 }
 
 func (accessLogger *HttpAccessLogger) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
@@ -27,21 +36,35 @@ func (accessLogger *HttpAccessLogger) ServeHTTP(rw http.ResponseWriter, rq *http
 	accessLogger.nextHandler.ServeHTTP(logResponseWriter, rq)
 	timeAfterServe := time.Now().UnixMilli()
 
+	var entries []slog.Attr
+
+	if accessLogger.options.LogClientIp {
+		entries = append(
+			entries,
+			slog.String("Client IP", strings.Split(rq.RemoteAddr, ":")[0]),
+		)
+	}
+
+	entries = append(
+		entries, []slog.Attr{
+			slog.String("Method", rq.Method),
+			slog.String("Host", rq.Host),
+			slog.String("Path", rq.URL.Path),
+			slog.String("Query", rq.URL.RawQuery),
+			slog.String("Protocol", rq.Proto),
+			slog.String("User Agent", rq.UserAgent()),
+			slog.String("Response Status Code", strconv.Itoa(logResponseWriter.StatusCode())),
+			slog.String(
+				"Duration (s)",
+				fmt.Sprintf("%.2f", float64(timeAfterServe-timeBeforeServe)/1000),
+			),
+		}...,
+	)
+
 	accessLogger.logger.LogAttrs(
 		rq.Context(),
 		slog.LevelInfo,
 		AccessLogMessage,
-		slog.String("Client IP", strings.Split(rq.RemoteAddr, ":")[0]),
-		slog.String("Method", rq.Method),
-		slog.String("Host", rq.Host),
-		slog.String("Path", rq.URL.Path),
-		slog.String("Query", rq.URL.RawQuery),
-		slog.String("Protocol", rq.Proto),
-		slog.String("User Agent", rq.UserAgent()),
-		slog.String("Response Status Code", strconv.Itoa(logResponseWriter.StatusCode())),
-		slog.String(
-			"Duration (s)",
-			fmt.Sprintf("%.2f", float64(timeAfterServe-timeBeforeServe)/1000),
-		),
+		entries...,
 	)
 }
--- infrastructure/http/router/middleware/access_test.go
+++ infrastructure/http/router/middleware/access_test.go
@@ -58,6 +58,7 @@ func (suite *AccessSuite) TestAccessLoggerCanLogAccessDetails() {
 			},
 		),
 		logger,
+		AccessLogOptions{true},
 	)
 
 	middleware.ServeHTTP(
